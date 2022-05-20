9a167817335a304cce979bf7bf644d028a446f93
luzbarreto20 <82717444+LuzBarreto20@users.noreply.github.com>
2022-05-20 11:42:36 -0300

    Agregando el Proyecto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Productos from './components/Productos'
+import Layout from './components/Layout'
+import Navbar from './components/Navbar'
+import Title from './components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      { name: 'Hibiscus', price: 250, img: './producto/mascabo.jpg'},
+      { name: 'Edulcorante', price: 500, img: './producto/nueces-mariposa-blanca-premium.jpg'},
+      { name: 'Pasta de mani', price: 200, img: './producto/tomate (1).jpg'},
+      
+      
+     
+
+    ]
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Layout>
+          <Title />
+        <Productos
+        agregarAlCarro={() => console.log('No hace nada')}
+        productos={this.state.productos} />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
