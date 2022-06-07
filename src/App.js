919618fb1035226ae20b7a1227d556f4fbfd0152
Cesar Perozo <30641619+cesarperozo@users.noreply.github.com>
2022-06-07 20:57:21 -0300

    refactor cofing

--- src/App.js
+++ src/App.js
@@ -1,70 +1,89 @@
-import { Component } from 'react';
-import { Button } from '@material-ui/core';
-import Productos from './components/Productos'
-import Layout from './components/Layout'
-import Navbar from './components/Navbar'
-import Title from './components/Title'
+import { Component } from "react";
+import { Button } from "@material-ui/core";
+import Productos from "./components/Productos";
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
+import Title from "./components/Title";
 
 class App extends Component {
   state = {
     productos: [
-      { name: 'Hibiscus', price: 250, img: './producto/hibiscus-tea.png'},
-      { name: 'Edulcorante', price: 500, img: './producto/nueces-mariposa-blanca-premium.jpg'},
-      { name: 'Pasta de mani', price: 200, img: './producto/tomate1.jpg'},
-      { name: 'chocolate', price: 250, img: './producto/arbejas.jpg'},
-      { name: 'mascabo', price: 250, img: './producto/mascabo.jpg'}, 
-      { name: 'pasta', price: 250, img: './producto/pasta-de-mani.jpg'},
-      { name: 'pasta', price: 250, img: './producto/pasta-de-mani.jpg'},
+      { name: "Hibiscus", price: 250, img: "./producto/hibiscus-tea.png" },
+      {
+        name: "Edulcorante",
+        price: 500,
+        img: "./producto/nueces-mariposa-blanca-premium.jpg",
+      },
+      { name: "Pasta de mani", price: 200, img: "./producto/tomate1.jpg" },
+      { name: "chocolate", price: 250, img: "./producto/arbejas.jpg" },
+      { name: "mascabo", price: 250, img: "./producto/mascabo.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+           { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
+      { name: "pasta", price: 250, img: "./producto/pasta-de-mani.jpg" },
     ],
     carro: [],
     esCarroVisible: false,
-  }
-  
+  };
 
   agregarAlCarro = (producto) => {
-    const { carro } = this.state
-    if (carro.find(x => x.name === producto.name)) {
-      const newCarro = carro.map(x => x.name === producto.name
-        ? ({
-          ...x,
-          cantidad: x.cantidad + 1
-        })
-        : x)
-        return this.setState({ carro: newCarro })
+    const { carro } = this.state;
+    if (carro.find((x) => x.name === producto.name)) {
+      const newCarro = carro.map((x) =>
+        x.name === producto.name
+          ? {
+              ...x,
+              cantidad: x.cantidad + 1,
+            }
+          : x
+      );
+      return this.setState({ carro: newCarro });
     }
     return this.setState({
       carro: this.state.carro.concat({
         ...producto,
         cantidad: 1,
-      })
-    })
-  }
+      }),
+    });
+  };
 
   mostrarCarro = () => {
-    if(this.state.carro.length) {
-      return
+    if (this.state.carro.length) {
+      return;
     }
-    this.setState({ esCarroVisible: !this.state.esCarroVisible})
-  }
+    this.setState({ esCarroVisible: !this.state.esCarroVisible });
+  };
   render() {
-    const { esCarroVisible } = this.state
+    const { esCarroVisible } = this.state;
     return (
       <>
-        <Navbar 
+      <Navbar 
         carro={this.state.carro} 
         esCarroVisible={esCarroVisible} 
         mostrarCarro={this.mostrarCarro} />
-        <Title />
+        <Title /> 
         <Layout>
-          
-          
-       
-        <Productos
-        agregarAlCarro={this.agregarAlCarro}
-        productos={this.state.productos} />
+          <Productos
+            agregarAlCarro={this.agregarAlCarro}
+            productos={this.state.productos}
+          />
         </Layout>
       </>
-    )
+    );
   }
 }
 
--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,70 +1,71 @@
 import { Component } from "react";
 import { Grid, Item } from "@mui/material";
-import { makeStyles } from '@material-ui/core/styles';
+import Box from "@mui/material/Box";
 
-import Button from "./Button";
+import { makeStyles } from "@material-ui/core/styles";
 
-const styles = {
-  producto: {
-    border: "solid 1px #eee",
-    boxShadow: "0 5px 5px rgba(0,0,0,0.1) ",
-     width: "auto",
-    padding: "10px 15px",
-    borderRadius: "5px",
-    margin: "10px",
-    backgroundColor: "red",
-    
+import { Button, Typography } from "@material-ui/core";
+
+const ProductoStyles = makeStyles({
+  container: {
+    borderRadius: 8,
+    boxShadow: "0 25px 50px -12px rgb(0 0 0 / 0.25)",
+    width: "300px",
+    minHeight: "350px",
+    overflow: "hidden",
+  },
+  containerBotton: {
+    padding: "14px",
   },
   img: {
     width: "100%",
     height: "350px",
-    objectFit: 'cover',
-  },
-
-};
-
-const ProductoStyles = makeStyles({
-  container: {
-   /*display: 'flex',
-    flexDirection: 'row',*/
-    /*boxShadow: '0 25px 50px -12px rgb(0 0 0 / 0.25)',*/
-    background: 'blue',
-    /*padding: '16px',
-    margin: '20px',
-    width: '40px',*/
-    border: '1px solid lightGrey',
-    width:'250px',
-    minHeight: '350px',
-
+    objectFit: "cover",
   },
 });
 
-
-
-
-export const Producto = ({agregarAlCarro, producto}) => {
+export const Producto = ({ agregarAlCarro, producto }) => {
   const classes = ProductoStyles();
-  
-    return (
-      <Grid  /*className={classes.container}*/ item xl={3}> 
-       <Grid xs={8}>
-          <p>xs=8</p>
-        </Grid>
-        <Grid item xs={4}>
-          <p>xs=4</p>
-        </Grid>
-       
-        
 
-        {/*  <img   alt={producto.name} src={producto.img} style={styles.img} /> 
-        <h3>{producto.name} </h3>
-        <p>{producto.price} </p>
-        <Button onClick={() => agregarAlCarro(producto)}>
-          Agregar al Carro
-        </Button>
-         */}
-      </Grid>
-    );
-  }
+  return (
+    <Grid item xs={12} sm={6} md={3} xl={3}>
+      <Box className={classes.container}>
+        <img alt={producto.name} src={producto.img} className={classes.img} />
+        <Grid
+          container
+          flexDirection="column"
+          className={classes.containerBotton}
+        >
+          <Grid
+            container
+            pb="12px"
+            flexDirection="row"
+            justifyContent="space-between"
+          >
+            <Grid item>
+              <Typography style={{ fontWeight: 400, fontSize: 18 }}>
+                {producto.name}{" "}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography style={{ fontWeight: 800, fontSize: 18 }}>
+                {" "}
+                {producto.price} $
+              </Typography>
+            </Grid>
+          </Grid>
+          <Button
+            style={{ background: "rgba(145, 195, 0, 0.9)" }}
+            onClick={() => agregarAlCarro(producto)}
+            variant="contained"
+            size="small"
+          >
+            Agregar al Carro
+          </Button>
+        </Grid>
+      </Box>
+    </Grid>
+  );
+};
 
 export default Producto;
